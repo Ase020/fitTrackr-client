547fa4fd3556d4a04d3860e4ee6a856cfa194b83
felixNyalenda <felixasejunior@gmail.com>
2023-07-30 01:45:43 +0300

    exercise details build

--- src/pages/exercise-details/ExerciseDetails.jsx
+++ src/pages/exercise-details/ExerciseDetails.jsx
@@ -1,4 +1,4 @@
-import { bodyPart, equipment, exerciseGif } from "../../assets";
+import { bodyPart, equipment, exerciseDesc, exerciseGif } from "../../assets";
 import "./exercise-details.css";
 
 const ExerciseDetails = () => {
@@ -26,25 +26,32 @@ const ExerciseDetails = () => {
                 <img src={equipment} alt="equipment" className="title-img" />
                 <p className="title-name">Equipment</p>
               </div>
-              <div className="exercise-detail-title">
-                Smith Machine Smith Machine
+              <div className="exercise-detail-desc">
+                Smith Machine Smith Machine Smith Machine Smith Machine
               </div>
             </div>
 
             <div className="exercise-detail-wrapper">
               <div className="exercise-detail-title">
                 <img src={bodyPart} alt="equipment" className="title-img" />
-                <p className="title-name">Equipment</p>
+                <p className="title-name">Body Part</p>
               </div>
-              <div className="exercise-detail-title">Smith Machine</div>
+              <div className="exercise-detail-desc">Smith Machine</div>
             </div>
 
             <div className="exercise-detail-wrapper">
               <div className="exercise-detail-title">
-                <img src={equipment} alt="equipment" className="title-img" />
-                <p className="title-name">Equipment</p>
+                <img src={exerciseDesc} alt="equipment" className="title-img" />
+                <p className="title-name">Description</p>
               </div>
-              <div className="exercise-detail-title">Smith Machine</div>
+              <div className="exercise-detail-desc">
+                Diamond Push-Up The diamond push-up variation targets the
+                triceps brachii.6 It is done with your hands close together and
+                the index fingers and thumbs of one hand touching the other
+                hand, making a diamond shape on the floor. You then do push-ups
+                with your hands touching the center of your chest and elbows
+                close to your sides during each rep.
+              </div>
             </div>
           </div>
         </div>
