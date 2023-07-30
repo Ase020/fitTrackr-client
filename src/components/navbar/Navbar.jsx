6f794595e62e1b4dee325c0b82ff788a97026c74
felixNyalenda <felixasejunior@gmail.com>
2023-07-30 14:17:23 +0300

    user profile dropdown build

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
 import { logo } from "../../assets";
 import "./navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [profile, setProfile] = useState(false);
+  const currentUser = {
+    name: "felix",
+    profile:
+      "https://images.pexels.com/photos/17751042/pexels-photo-17751042.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  };
   return (
     <header className="navbar-container">
       {/* logo */}
@@ -22,14 +29,36 @@ const Navbar = () => {
       </nav>
 
       {/* login */}
-      <div className="logon-options">
-        <Link to="/login" className="logon-btn login-option">
-          Log In
-        </Link>
-        <Link to="/signup" className="logon-btn signup-option">
-          Sign Up
-        </Link>
-      </div>
+      {currentUser ? (
+        <div className="user_profile">
+          <img
+            src={currentUser.profile}
+            alt=""
+            className="user_profile-pic"
+            onClick={() => setProfile((prevState) => !prevState)}
+          />
+          {profile && (
+            <div className="user_profile-container">
+              <Link className="user_profile-link" to="/profile">
+                My profile
+              </Link>
+              <Link className="user_profile-link" to="/dashboard">
+                Dashboard
+              </Link>
+              <button className="user_profile-btn">Logout</button>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="logon-options">
+          <Link to="/login" className="logon-btn login-option">
+            Log In
+          </Link>
+          <Link to="/signup" className="logon-btn signup-option">
+            Sign Up
+          </Link>
+        </div>
+      )}
     </header>
   );
 };
