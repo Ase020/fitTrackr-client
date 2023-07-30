0a260b7cda1c1bf2fe89109fbf17ce957f98baa9
Milhan Osman <milhanomarosman@gmail.com>
2023-07-30 16:12:44 +0300

    rendering the card

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
-import { default as Navbar } from "./navbar/Navbar";
-import { default as Footer } from "./footer/Footer";
-import { default as HeroSection } from "./hero-section/HeroSection";
-import { default as CleanDesign } from "./clean-design/CleanDesign";
+import { default as Navbar } from './navbar/Navbar';
+import { default as Footer } from './footer/Footer';
+import { default as HeroSection } from './hero-section/HeroSection';
+import { default as CleanDesign } from './clean-design/CleanDesign';
+import { default as ExerciseCard } from './exerciseCard/ExerciseCard';
 
-export { Navbar, Footer, HeroSection, CleanDesign };
+export { Navbar, Footer, HeroSection, CleanDesign, ExerciseCard };
