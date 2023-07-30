851a9133f8bc8f641ad9cc5640e69be03db49728
brianrono <brianrono16@gmail.com>
2023-07-30 23:59:38 +0300

    Signup build

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,47 +1,62 @@
 import "./Signup.css";
 import { signUp } from "../../assets";
+import { googleIcon } from "../../assets";
 
 const Signup = () => {
   return (
     <div className="signup-container">
       <div className="signup-wrapper">
-        <div className="image-container">
-          <img
-            src={signUp}
-            alt="Signup-image"
-            className="signup-image-container-image"
-          />
-        </div>
+          <div className="image-container">
+            <img
+              src={signUp}
+              alt="Signup-image"
+              className="signup-image-container-image"
+            />
+          </div>
 
         <div className="form-container">
           <h2>Sign up</h2>
           <form>
             <div className="form-group">
               <label htmlFor="email">Email Address</label>
-              <input type="email" id="email" name="email" required />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="jdoe123@example.com"
+                required
+              />
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" name="password" required />
-            </div>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                required
+              />
+              </div>
             <div className="form-group">
               <label htmlFor="confirmPassword">Confirm Password</label>
               <input
                 type="password"
                 id="confirmPassword"
                 name="confirmPassword"
+                placeholder="Confirm your password"
                 required
               />
             </div>
             <button type="submit">Signup</button>
           </form>
-            <div className="or-divider">
-              <hr />
-                <p>or</p>
-            </div>
+          <div id="or">or</div>
           <div className="google-signup">
+            <img
+              src={googleIcon}
+              alt="Google-icon"
+              className="google-icon"
+            />
             <p>Google</p>
-            {/* not forgetting the google identity */}
           </div>
           <p>
             Already have an account?{" "}
