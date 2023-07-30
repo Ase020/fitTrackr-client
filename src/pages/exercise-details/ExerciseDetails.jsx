95d8621949a7146cb2812936fa16876e8d91e880
felixNyalenda <felixasejunior@gmail.com>
2023-07-30 12:37:39 +0300

    related exercise section build

--- src/pages/exercise-details/ExerciseDetails.jsx
+++ src/pages/exercise-details/ExerciseDetails.jsx
@@ -2,7 +2,25 @@ import { bodyPart, equipment, exerciseDesc, exerciseGif } from "../../assets";
 import "./exercise-details.css";
 
 const RelatedExercise = () => (
-  <div className="related_exercise-wrapper">related</div>
+  <div className="related_exercise-wrapper">
+    <img src={exerciseGif} alt="" className="related_exercise-img_wrapper" />
+    <div className="relatd_exercise-name-container">
+      <p className="related_exercise-title">45° side bend</p>
+      <div className="body_part_equipment-container">
+        <div className="body_part-wrapper">
+          <p className="exercise_body_part-header">
+            Body part: <span className="body_part-data">Waist</span>
+          </p>
+        </div>
+
+        <div className="body_part-wrapper">
+          <p className="exercise_body_part-header">
+            Body part: <span className="body_part-data">Waist</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 const ExerciseDetails = () => {
