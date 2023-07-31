0397093450a7ced79e37178f12912e1ddf571533
felixNyalenda <felixasejunior@gmail.com>
2023-07-31 17:48:38 +0300

    dashboard mui add

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,3 +1,5 @@
+import DownloadIcon from "@mui/icons-material/Download";
+
 import "./dashboard.css";
 
 const Dashboard = () => {
@@ -5,7 +7,10 @@ const Dashboard = () => {
     <div className="dashboard_container">
       <div className="dashboard_header-container">
         <h4 className="dashboard_header">My Dashboard</h4>
-        <button className="dashboard_download-reports">Download Reports</button>
+        <button className="dashboard_download-reports">
+          <DownloadIcon />
+          <p className="dashboard_download-p">Download Reports</p>
+        </button>
       </div>
       <div className="dashboard_report-section">reports section</div>
     </div>
