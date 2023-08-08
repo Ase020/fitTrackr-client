16f94675e8869a55cf506879793c5d77afde9e9b
felixNyalenda <felixasejunior@gmail.com>
2023-08-08 19:26:26 +0300

    table init

--- src/pages/manage-users/ManageUsers.jsx
+++ src/pages/manage-users/ManageUsers.jsx
@@ -1,7 +1,383 @@
 import "./manage-users.css";
 
 const ManageUsers = () => {
-  return <div>ManageUsers</div>;
+  return (
+    <main className="manage_users">
+      <h4 className="manage_users-headers">
+        Manage app users with a single click
+      </h4>
+
+      <div className="manage_user-table">
+        <table className="mange_users_table-container">
+          <thead className="thead">
+            <tr>
+              <th>Email</th>
+              <th>Username</th>
+              <th>Gender</th>
+              <th>Age</th>
+              <th>BMI</th>
+              <th>Admin?</th>
+              <th>Date Joined</th>
+              <th>Save</th>
+            </tr>
+          </thead>
+
+          <tbody className="tbody">
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="td_email" data-label="Email">
+                felixasejunior@example.com
+              </td>
+              <td className="td_username" data-label="Username">
+                felix.asejunior
+              </td>
+              <td className="td_gender" data-label="Gender">
+                Female
+              </td>
+              <td className="td_age" data-label="Age">
+                25
+              </td>
+              <td className="td_bmi" data-label="BMI">
+                19.35
+              </td>
+              <td className="td_admin" data-label="Admin?">
+                <input type="checkbox" className="td_input-checkbox" />
+              </td>
+              <td className="td_date" data-label="Date Joined">
+                2023-08-08
+              </td>
+              <td className="td_save" data-label="Save">
+                <button className="td_save-btn">Save</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  );
 };
 
 export default ManageUsers;
