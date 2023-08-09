5dca91d47c5050554b11c57d583179a7f69729ce
felixNyalenda <felixasejunior@gmail.com>
2023-08-10 00:57:14 +0300

    eslint

--- src/pages/fitness/Fitness.jsx
+++ src/pages/fitness/Fitness.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect, useState } from "react";
 import { FitnessCard, FitnessRecord } from "../../components";
 import "./fitness.css";
